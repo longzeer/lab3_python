d29f244d381b96b04dad4ad0ba7b20a9d8657eb5
longzeer <shao0015@algonquinlive.com>
2019-09-23 08:31:50 +0800

    Yong's lab3

---
+++ question10_Lab3_Yong.py
@@ -0,0 +1,7 @@
+## question 10 solution ##
+
+m = int(input ('What is the number of miles driven?'))
+g = int(input ('What is the number of gallons used?'))
+MPG = m / g
+print('The MPG is')
+print(MPG)
---
+++ question8_Lab3_Yong.py
@@ -0,0 +1,7 @@
+## question 8 solution ##
+
+from math import pi
+Radius = int(input ("what is the radius?"))
+Area = pi*Radius**2
+print("Dear user the area is")
+print(Area)
